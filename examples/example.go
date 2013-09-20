1dc5c165b67e3e309f7bcc2e9c108eaea066d5be
Mark Story <mark@mark-story.com>
2013-09-19 23:21:31 -0400

    Add an example consumer.
    Still not sure if this is going to work at all.

---
+++ examples/example.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"consumer"
+	"flag"
+	"log"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	var ini = flag.String("config", "./test.ini", "The configuration file to use.")
+
+	c, err := consumer.Create(ini)
+	if err != nil {
+		log.Fatalf("Unable to create consumer")
+	}
+	c.Consume(worker)
+}
+
+// Example worker function
+// Should eventually get type coming out of consumer
+// instead of an amqp.Delivery value.
+func worker (msg amqp.Delivery) {
+	log.Printf("msg received")
+	msg.Ack()
+}
