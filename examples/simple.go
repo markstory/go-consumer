76a7614e4e50bf6a6bfbd896cf7342b55b9336a6
Mark Story <mark@mark-story.com>
2013-09-30 20:31:45 -0400

    Rename example files.
    Rename the test.ini to simple, as another example ini file will be added
    for multiple queue configuration.

--- examples/simple.go
+++ examples/simple.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	var ini = flag.String("config", "./test.ini", "The configuration file to use.")
+	var ini = flag.String("config", "./simple.ini", "The configuration file to use.")
 
 	c, err := consumer.Create(*ini)
 	if err != nil {
@@ -23,8 +23,13 @@ func main() {
 // Example worker function
 // Just prints to the logs and sleeps between receiving messages.
 func worker(msg *consumer.Message) {
-	log.Printf("msg received %s", msg.Body)
+	log.Printf(
+		"Message received Exchange: %s, RoutingKey: %s, Body: %s",
+		msg.Exchange,
+		msg.RoutingKey,
+		msg.Body)
+
 	msg.Ack(true)
-	time.Sleep(2000 * time.Millisecond)
-	log.Print("woke up")
+	time.Sleep(1000 * time.Millisecond)
+	log.Print("sleep complete")
 }
